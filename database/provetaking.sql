8ac3b6cb70e3f396972a169343ad7b75126894a5
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2022-10-25 13:46:45 +0200

    Some db stuff and strapi

---
+++ database/provetaking.sql
@@ -0,0 +1,10 @@
+SET client_encoding = 'UTF8';
+
+CREATE TABLE prove(
+    prove_id BIGSERIAL PRIMARY KEY,
+    beskrivelse VARCHAR,
+    pos geography
+    );
+
+
+COMMIT;
---
+++ database/sampledata.sql
@@ -0,0 +1,11 @@
+-- Pos == Kontorsted Brummundal 
+insert into prove(beskrivelse, pos) values('Kontorsted Brummundal', 'SRID=4326;POINT(10.929107 60.87755)');
+
+-- Pos == Esso Brummundal
+insert into prove(beskrivelse, pos) values('Esso Brummundal', 'SRID=4326;POINT(10.931786 60.879543)');
+
+
+insert into prove(beskrivelse, pos) values('Stupetårnet i Hamar', 'SRID=4326;POINT(11.059909 60.792975)');
+
+
+commit;
