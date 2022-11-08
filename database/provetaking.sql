d073d6aeaded127cd0994d78149a0527dc47a101
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2022-11-08 15:00:38 +0100

    Endring av gis data etter samtale med Turid

--- database/provetaking.sql
+++ database/provetaking.sql
@@ -3,7 +3,7 @@ SET client_encoding = 'UTF8';
 CREATE TABLE prove(
     prove_id BIGSERIAL PRIMARY KEY,
     beskrivelse VARCHAR,
-    pos geography
+    pos geometry
     );
 
 
--- database/sampledata.sql
+++ database/sampledata.sql
@@ -1,11 +1,11 @@
 -- Pos == Kontorsted Brummundal 
-insert into prove(beskrivelse, pos) values('Kontorsted Brummundal', 'SRID=4326;POINT(10.929107 60.87755)');
+insert into prove(beskrivelse, pos) values('Kontorsted Brummundal', 'SRID=EPSG3857;POINT(10.929107 60.87755)');
 
 -- Pos == Esso Brummundal
-insert into prove(beskrivelse, pos) values('Esso Brummundal', 'SRID=4326;POINT(10.931786 60.879543)');
+insert into prove(beskrivelse, pos) values('Esso Brummundal', 'SRID=EPSG3857;POINT(10.931786 60.879543)');
 
 
-insert into prove(beskrivelse, pos) values('Stupetårnet i Hamar', 'SRID=4326;POINT(11.059909 60.792975)');
+insert into prove(beskrivelse, pos) values('Stupetårnet i Hamar', 'SRID=EPSG3857;POINT(11.059909 60.792975)');
 
 
 commit;
